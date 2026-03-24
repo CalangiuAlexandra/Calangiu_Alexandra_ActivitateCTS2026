85592b3b090a07840a4fb258735d62e2c016e0b0
Calangiu Alexandra <calangiualexandra22@stud.ase.ro>
2026-03-24 23:33:37 +0200

    Implementare Facade pentru AgeTur - MAin

---
+++ FataAgeTur/src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import classes.RezervareFacade;
+
+public class Main {
+    public static void main(String[] args) {
+        RezervareFacade facade = new RezervareFacade();
+        facade.rezervaPachet();
+    }
+}
