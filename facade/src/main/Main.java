cf3d1ee61712484fc25258e60a873a670a94fa89
Calangiu Alexandra <calangiualexandra22@stud.ase.ro>
2026-05-09 02:58:09 +0300

    facade - testare main

---
+++ facade/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import model.Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.organizeazaNunta();
+    }
+}
